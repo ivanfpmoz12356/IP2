7870d14e3c2fbb7b4d6b0dac46fa9a59ae59578d
ivanfpmoz12356 <49004393+ivanfpmoz12356@users.noreply.github.com>
2022-09-13 10:26:21 +0200

    First

---
+++ src/views/KomentarView.vue
@@ -0,0 +1,18 @@
+<template>
+<FormComp></FormComp>
+</template>
+
+<script>
+import FormComp from "../components/FormComp.vue";
+
+export default {
+  components:{ FormComp},
+      data(){
+        return{
+          addDialog: false,
+          successAlert: false 
+        }
+      }
+    
+  }
+</script>
